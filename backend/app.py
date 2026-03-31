e4fed2dfe737b86840281389e3ad496477e9fd4c
Karan Shelar <karanshelar8775@gmail.com>
2026-03-31 18:53:53 +0530

    Feature: Completed Backend with Redis connection

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,26 @@
+import os
+import redis
 from fastapi import FastAPI
 
-app= FastAPI(tittle= "K8s Microservices Lab")
+app= FastAPI(title= "K8s Microservices Lab")
+
+REDIS_HOST= os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT= int(os.getenv("REDIS_PORT", 6379))
+
+# INITIALIZE REDIS
+r= redis.Redis(host= REDIS_HOST, port= REDIS_PORT, decode_responses= True)
 
 @app.get("/health")
 def health_check():
-    return {"status": "healthy"}
+    """Confirms the API and Redis Connection are healthy."""
+    try:
+        r.ping()
+        return{"status": "healthy", "redis": "connected"}
+    except Exception as e:
+        return{"status": "unhealthy", "redis": str(e)}
+
+@app.post("/tasks")
+def create_task(message: str):
+    """Pushes a message into the Redis List 'task_queue'."""
+    r.lpush("task_queue", message)
+    return{"status": "task_received", "message": message}
